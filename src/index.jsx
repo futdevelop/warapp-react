8cbc4f2b58ad60db8dadd6b083bed29cf4f0a343
Kolya Melnichuk <nikolfutdeveloper@gmail.com>
2024-07-13 01:16:32 +0200

    .

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,14 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './store/index.js';
-
-import './language/18n.js';
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App.jsx'
+import './index.css'
+import './language/18n.js'
+import store from './store/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	//  <React.StrictMode>
-	<Provider store={store}>
-		<App />
-	</Provider>
-	//  </React.StrictMode>,
-);
+  //  <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  //  </React.StrictMode>,
+)
