7f5736609ff3f275ab53fc90d314b32917c8dc36
Kolya Melnichuk <nikolfutdeveloper@gmail.com>
2024-07-13 01:02:02 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,46 @@
-import { useCallback, useEffect } from 'react';
-import WarInfo from './containers/WarInfoContainer';
-import Header from './containers/HeaderContainer';
-import Footer from './containers/FooterContainer';
-import { useSelector, useDispatch } from 'react-redux';
-import PulseLoader from 'react-spinners/PulseLoader';
-import { fetchWarStats } from './store/apiSlice';
-import { apiSlice } from './store/selectors';
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import PulseLoader from 'react-spinners/PulseLoader'
+import Footer from './containers/FooterContainer'
+import Header from './containers/HeaderContainer'
+import WarInfo from './containers/WarInfoContainer'
+import { fetchWarStats } from './store/apiSlice'
+import { apiSlice } from './store/selectors'
 
 const App = () => {
-	const { dataLoaded, statusLoading } = useSelector(apiSlice);
+  const { dataLoaded, statusLoading } = useSelector(apiSlice)
+  const dispatch = useDispatch()
 
-	console.log('render');
-	const dispatch = useDispatch();
-	
-	useEffect(() => {
-		!dataLoaded && dispatch(fetchWarStats());
-	}, [dataLoaded, dispatch]);
+  useEffect(() => {
+    !dataLoaded && dispatch(fetchWarStats())
+  }, [dataLoaded, dispatch])
 
-	if (statusLoading === 'idle') {
-		return (
-			<div className='app'>
-				<Header />
-				<WarInfo />
-				<Footer />
-			</div>
-		);
-	} else if (statusLoading === 'loading') {
-		return (
-			<div className='war-info__loading'>
-				<PulseLoader
-					color={'#414A4E'}
-					className='loading'
-					size={30}
-					aria-label='Loading Spinner'
-					data-testid='loader'
-				/>
-			</div>
-		);
-	} else
-		return (
-			<>
-				<h1>Error</h1>
-			</>
-		);
-};
+  if (statusLoading === 'idle') {
+    return (
+      <div className='app'>
+        <Header />
+        <WarInfo />
+        <Footer />
+      </div>
+    )
+  } else if (statusLoading === 'loading') {
+    return (
+      <div className='war-info__loading'>
+        <PulseLoader
+          color={'#414A4E'}
+          className='loading'
+          size={30}
+          aria-label='Loading Spinner'
+          data-testid='loader'
+        />
+      </div>
+    )
+  } else
+    return (
+      <>
+        <h1>Error</h1>
+      </>
+    )
+}
 
-export default App;
+export default App
